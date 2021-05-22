a9493ed0a9eb1857c8272d095e1c90cb4fff925c
IlhomMusayev <ilhommusayev2001@gmail.com>
2021-05-22 22:09:15 +0500

    Errorlar tuzatildi

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const searchSuraBtnElement = document.querySelector('.btn')
 const quranAudioElement = document.querySelector('.quranAudio')
-
-searchSuraBtnElement.addEventListener('click', e => {
+const errorElement = document.querySelector('.error')
+// searchSuraBtnElement.addEventListener('click', e => {
+//     let suraNumberElement = document.querySelector('#suraNumber').value
+//     if(suraNumberElement <= 114){
+//         if (suraNumberElement.length == 1) {
+//             quranAudioElement.src = `http://server16.mp3quran.net/deban/Rewayat-Hafs-A-n-Assem/00` + suraNumberElement + `.mp3`
+//         } else if (suraNumberElement.length == 2) {
+//             quranAudioElement.src = `http://server16.mp3quran.net/deban/Rewayat-Hafs-A-n-Assem/0` + suraNumberElement + `.mp3`
+//         } else if (suraNumberElement.length == 3) {
+//             quranAudioElement.src = `http://server16.mp3quran.net/deban/Rewayat-Hafs-A-n-Assem/` + suraNumberElement + `.mp3`
+//         }
+//     }else{
+//         errorElement.textContent = 'Bunday tartib raqamidagi sura mavjud emas!!! \n114 dan kichik son kiriting!'
+//     }
+// })
+document.addEventListener('keyup', e => {
     let suraNumberElement = document.querySelector('#suraNumber').value
-    if(suraNumberElement.length == 1){
-        quranAudioElement.src = `http://server16.mp3quran.net/deban/Rewayat-Hafs-A-n-Assem/00`+suraNumberElement+`.mp3`
-    }
-    else if(suraNumberElement.length == 2){
-        quranAudioElement.src = `http://server16.mp3quran.net/deban/Rewayat-Hafs-A-n-Assem/0`+suraNumberElement+`.mp3`
-    }
-    else if(suraNumberElement.length == 3){
-        quranAudioElement.src = `http://server16.mp3quran.net/deban/Rewayat-Hafs-A-n-Assem/`+suraNumberElement+`.mp3`
+    if (suraNumberElement <= 114) {
+        if (suraNumberElement.length == 1) {
+            quranAudioElement.src = `https://server16.mp3quran.net/deban/Rewayat-Hafs-A-n-Assem/00` + suraNumberElement + `.mp3`
+            errorElement.textContent = ''
+        } else if (suraNumberElement.length == 2) {
+            quranAudioElement.src = `https://server16.mp3quran.net/deban/Rewayat-Hafs-A-n-Assem/0` + suraNumberElement + `.mp3`
+            errorElement.textContent = ''
+        } else if (suraNumberElement.length == 3) {
+            quranAudioElement.src = `https://server16.mp3quran.net/deban/Rewayat-Hafs-A-n-Assem/` + suraNumberElement + `.mp3`
+            errorElement.textContent = ''
+        }
+    }else{
+        errorElement.textContent = 'Bunday tartib raqamidagi sura mavjud emas!!! \n114 dan kichik son kiriting!'
     }
 })
 
